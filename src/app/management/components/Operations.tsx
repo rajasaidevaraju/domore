e64389ae82435296d4052612133887a38817a640
Raja <rajasaidevaraju@yahoo.com>
2025-03-26 12:01:14 -0400

    feat: add repair functionality and enhance UI tooltips in operations component

--- src/app/management/components/Operations.tsx
+++ src/app/management/components/Operations.tsx
@@ -11,7 +11,10 @@ interface OperationsProps {
 const Operations: React.FC<OperationsProps> = ({showToast}:OperationsProps) => {
 
     const [isScanning, setIsScanning] = useState(false);
+    const [isRepair,setIsRepair]=useState(false);
+
     let scanningToolTip= isScanning?"Scanning in progress":undefined;
+    let repairToolTip=isRepair?"Repair in progress":undefined;
     const handleScan = async () => {
         setIsScanning(true);
         try{
@@ -41,8 +44,28 @@ const Operations: React.FC<OperationsProps> = ({showToast}:OperationsProps) => {
        
     };
 
-    const handleMigrate = () => {
-        console.log("Migrating file paths...");
+    const handleReset = async () => {
+       
+        setIsRepair(true);
+        try{
+            let token=localStorage.getItem('token');
+            if(token===null){
+                showToast("Unauthorized access", MessageType.DANGER);
+            }else{
+                let data= await OperationRequests.fetchRepair(token);
+                showToast(data.message, MessageType.SUCCESS);
+            }
+       }catch(error){
+            if(error instanceof Error){
+                showToast(error.message, MessageType.DANGER);
+                
+            }else{
+                showToast('An error occurred on scan request.', MessageType.DANGER);
+                console.error('Error:', error);
+            }
+       }finally{
+            setIsRepair(false);
+       }
        
     };
 
@@ -67,8 +90,8 @@ const Operations: React.FC<OperationsProps> = ({showToast}:OperationsProps) => {
                 <p className={styles.explanation}>This will remove any db entries not found in the file system.</p>
             </div>
             <div className={styles.operationItem}>
-                <RippleButton  className={`${styles.commonButton} ${styles.minWidth}`} onClick={handleMigrate}>
-                    Migrate
+                <RippleButton suggestion={repairToolTip} className={`${styles.commonButton} ${styles.minWidth}`} onClick={handleReset}>
+                    Repair
                 </RippleButton>
                 <p className={styles.explanation}>Broken file paths will be searched and updated.</p>
             </div>
--- src/app/service/OperationRequests.ts
+++ src/app/service/OperationRequests.ts
@@ -16,5 +16,20 @@ export const OperationRequests = {
         let data=await response.json();
         return {message:data.message}
         
+    },
+
+    async fetchRepair(token:string):Promise<ApiResponse>{
+
+        const response = await fetch(`${API_BASE_URL}/server/repair`, { method: "PUT",headers: {'Authorization': `Bearer ${token}`}, });
+        
+        if (!response.ok) {
+            const error = await response.json().catch(() => null);
+            throw new Error(error?.message || "Failed to fetch items");
+        }
+
+        let data=await response.json();
+        return {message:data.message}
+        
     }
+
 }
