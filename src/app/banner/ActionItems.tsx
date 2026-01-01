78f6c2f6be173b515482df192fd6d91ccfa89fb4
rajasaidevaraju <rajasaidevaraju@yahoo.com>
2026-01-01 21:59:59 +0530

    removed change server

--- src/app/banner/ActionItems.tsx
+++ src/app/banner/ActionItems.tsx
@@ -8,39 +8,39 @@ import ChangePanel from './ChangePanel'
 import { usePathname, useRouter } from 'next/navigation';
 import { UserRequests } from '../service/UserRequests';
 import { useAuthStore } from '@/app/store/auth';
-interface ActionItemsProps{
-    isMobile:Boolean
-    closeMenu?:()=>void
+interface ActionItemsProps {
+    isMobile: Boolean
+    closeMenu?: () => void
 }
 
-function ActionItems({isMobile,closeMenu}:ActionItemsProps){
+function ActionItems({ isMobile, closeMenu }: ActionItemsProps) {
     const [showChangePanel, setChangePanel] = useState(false);
     const [isAnimating, setIsAnimating] = useState(false);
-    const { isLoggedIn,token,clearAuth } = useAuthStore();
+    const { isLoggedIn, token, clearAuth } = useAuthStore();
     const pathname = usePathname();
 
     const isManagementActive = pathname === '/management';
     const isFilterActive = pathname === '/filter';
     const isLoginActive = pathname === '/login';
-    
+
 
     const router = useRouter();
 
-    
+
     const handleLogout = async () => {
         if (!!token) {
             try {
-                await UserRequests.logoutUser(token);  
+                await UserRequests.logoutUser(token);
             } catch (error) {
                 console.log('Logout failed:', error);
             }
-            finally{
+            finally {
                 closeMenu?.();
-                clearAuth();         
+                clearAuth();
                 router.push('/');
             }
         }
-        
+
     };
 
 
@@ -48,7 +48,7 @@ function ActionItems({isMobile,closeMenu}:ActionItemsProps){
         setChangePanel(true);
         setIsAnimating(true);
     };
-    
+
     const closePanel = () => {
         setIsAnimating(false);
         setTimeout(() => {
@@ -57,17 +57,17 @@ function ActionItems({isMobile,closeMenu}:ActionItemsProps){
         }, 300);
     };
 
-    
 
-    let style=`${styles['actionItems']} ${isMobile? styles['actionItemsMenu']:""}`
-    let buttonStyle=`${styles['actionItem']} ${isMobile? styles['actionItemMobile']:""}`
 
-    let filterStyle=`${buttonStyle} ${!isMobile &&isFilterActive? styles['active']:"" }`
-    let managementStyle=`${buttonStyle} ${!isMobile &&isManagementActive? styles['active']:"" }`
-    let loginStyle=`${buttonStyle} ${!isMobile &&isLoginActive? styles['active']:"" } ${isLoggedIn && styles['hidden']}`
-    let logoutStyle=`${buttonStyle} ${!isLoggedIn && styles['hidden']}`
+    let style = `${styles['actionItems']} ${isMobile ? styles['actionItemsMenu'] : ""}`
+    let buttonStyle = `${styles['actionItem']} ${isMobile ? styles['actionItemMobile'] : ""}`
 
-    return(
+    let filterStyle = `${buttonStyle} ${!isMobile && isFilterActive ? styles['active'] : ""}`
+    let managementStyle = `${buttonStyle} ${!isMobile && isManagementActive ? styles['active'] : ""}`
+    let loginStyle = `${buttonStyle} ${!isMobile && isLoginActive ? styles['active'] : ""} ${isLoggedIn && styles['hidden']}`
+    let logoutStyle = `${buttonStyle} ${!isLoggedIn && styles['hidden']}`
+
+    return (
         <>
             <div className={style}>
                 <PressableLink href="/filter" className={filterStyle} onClick={closeMenu}>
@@ -79,32 +79,32 @@ function ActionItems({isMobile,closeMenu}:ActionItemsProps){
                     <span className={styles.iconText}>Management</span>
                 </PressableLink>
                 {isLoggedIn ? (
-                <RippleButton className={logoutStyle} onClick={handleLogout}>
-                    <img src="/svg/logout.svg" alt="Logout" className={styles.icon} />
-                    <span className={styles.iconText}>Logout</span>
-                </RippleButton>                
+                    <RippleButton className={logoutStyle} onClick={handleLogout}>
+                        <img src="/svg/logout.svg" alt="Logout" className={styles.icon} />
+                        <span className={styles.iconText}>Logout</span>
+                    </RippleButton>
                 ) : (
-                <PressableLink href="/login" className={loginStyle} onClick={closeMenu}>
-                    <img src="/svg/login.svg" alt="Login" className={styles.icon} />
-                    <span className={styles.iconText}>Login</span>
-                </PressableLink>
+                    <PressableLink href="/login" className={loginStyle} onClick={closeMenu}>
+                        <img src="/svg/login.svg" alt="Login" className={styles.icon} />
+                        <span className={styles.iconText}>Login</span>
+                    </PressableLink>
                 )}
-                <RippleButton className={buttonStyle} onClick={openPanel}>
+                {/*<RippleButton className={buttonStyle} onClick={openPanel}>
                 <img src="/svg/switch.svg" alt="Change Server" className={styles.icon} />
                 <span className={styles.iconText}>Change Server</span>
-                </RippleButton>
+                </RippleButton>*/}
             </div>
             {showChangePanel && (
                 <div>
-                  <div className={`${styles.overlay} ${isAnimating ? styles.enter : styles.exit}`}></div>
-                  <div
-                    className={`${styles.changePanel} ${isAnimating ? styles.enter : styles.exit}`}
-                    onAnimationEnd={() => !showChangePanel && setIsAnimating(false)}
-                  >
-                    <ChangePanel onClose={closePanel}></ChangePanel>
-                  </div>
+                    <div className={`${styles.overlay} ${isAnimating ? styles.enter : styles.exit}`}></div>
+                    <div
+                        className={`${styles.changePanel} ${isAnimating ? styles.enter : styles.exit}`}
+                        onAnimationEnd={() => !showChangePanel && setIsAnimating(false)}
+                    >
+                        <ChangePanel onClose={closePanel}></ChangePanel>
+                    </div>
                 </div>
-                )}
+            )}
         </>
     )
 }
