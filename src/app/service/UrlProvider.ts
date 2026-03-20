5749f380659b1edc78853c0c8da2815db572aa1f
rajasaidevaraju <rajasaidevaraju@yahoo.com>
2026-03-20 21:23:52 +0530

    updated code to run with nginx

--- src/app/service/UrlProvider.ts
+++ src/app/service/UrlProvider.ts
@@ -1,20 +1,7 @@
 export function ServerUrlProvider(): string {
-    const serverAddress = process.env.NEXT_PUBLIC_SERVER_ADDRESS;
-    const serverFromEnvFlag = process.env.NEXT_PUBLIC_SERVER_FROM_ENV === "true";
-
+    const serverAddress = process.env.SERVER_ADDRESS;
     if (typeof window === "undefined") {
-        // Server-side (during SSR or build)
-        return serverFromEnvFlag ? (serverAddress ?? "http://localhost:1280") : "http://localhost:1280";
+        return serverAddress ?? "http://localhost:1280";
     }
-
-    // Client-side (browser)
-    /*if (serverFromEnvFlag && serverAddress) {
-        return serverAddress;
-    } else {
-        const hostname = window.location.hostname;
-        return `http://${hostname}:1280`;
-    }*/
-    const hostname = window.location.hostname;
-    const port = window.location.port;
-    return port ? `http://${hostname}:${port}` : `http://${hostname}`;
+    return window.location.origin;
 }
